0aa9a9cb66361db503c1888502ab270fb5a1ab59
Md. Jahid Hasan <itsjahidhasan@outlook.com>
2022-07-20 18:02:43 +0600

    axios added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,81 @@
-import React from 'react';
-import { TextField, Container, Button, Box } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { TextField, Container, Button, Box } from "@mui/material";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+
 export const LoginForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = (data) => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", {
+        title: data.title,
+        body: data.body,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+
+  async function onSubmitAsync(data) {
+    try {
+      const response = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          title: data.title,
+          body: data.body,
+        }
+      );
+      console.log(response);
+      console.log(watch("title"));
+      console.log(watch("body"));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
   return (
     <Container maxWidth="xs">
       <h1>hello</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmitAsync)}>
         <Box mb={2}>
           <TextField
             variant="outlined"
-            label="Email"
+            label="Title"
             fullWidth
-            autoComplete="Email"
+            autoComplete="Title"
             autoFocus
-            {...register('email', { required: 'Required' })}
-            error={!!errors?.email}
-            helperText={errors?.email ? errors.email.message : null}
+            {...register("title", { required: "Required" })}
+            error={!!errors?.title}
+            helperText={errors?.title ? errors.title.message : null}
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            variant="outlined"
+            label="Body"
+            fullWidth
+            autoComplete="Body"
+            autoFocus
+            {...register("body", { required: "Required" })}
+            error={!!errors?.body}
+            helperText={errors?.body ? errors.body.message : null}
           />
         </Box>
 
         <Button type="submit" variant="contained" fullWidth>
-          Log In
+          Submit
         </Button>
       </form>
     </Container>
